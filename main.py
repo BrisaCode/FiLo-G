805793fbb410a13010c1a8c879622423fb8651d1
Lucas Cotta <lucas.oliveira.cotta@gmail.com>
2015-06-22 14:17:47 -0400

    Testing open the main window

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 '''
 File Loader G
-June 22, 2015 
+June 22, 2015
 @author: Lucas Cotta
 
 Main program to the File Loader G application V0.1
 '''
 
 import sys 
-import StringIO
+import os
 import copy
 
-from main_window.py import Ui_FileLoaderG
+from PyQt4 import QtCore, QtGui
+from PyQt4.Qt import QMessageBox, QScrollArea
+
+from main_window import Ui_FileLoaderG
 
 
 # class for the main window
@@ -18,34 +21,36 @@ from main_window.py import Ui_FileLoaderG
 class Main(QtGui.QMainWindow):
     def __init__(self):
 
-        QtGui.QMainWindow.__init__(self):
-            # user interface
-            #
-            self.flg = Ui.FileLoaderF()
-            self.flg = setupUI(self)
+        QtGui.QMainWindow.__init__(self)
+        # user interface
+        #
+        self.flg = Ui_FileLoaderG()
+        self.flg.setupUi(self)
 
-            # get the current path of the server directory
-            #
-            self.server_directory = (str(sys.argv[0]) + "/server"
+        # get the current path of the server directory
+        #
+        self.server_directory = (str(sys.argv[0])) + "/server"
 
-            # get the current path of local directory
-            #
-            self.local_directory = (str(sys.argv[0])) + "/local"
+        # get the current path of local directory
+        #
+        self.local_directory = (str(sys.argv[0])) + "/local"
 
             
 # main function
 def main():
     
-    app = QtGui.Application([])
+    app = QtGui.QApplication([])
     window = Main()
     window.show()
     QtGui.QApplication.instance().exec_()
 
-    # remove seg fault
+    # remove segmentation fault
     #
     os._exit(0)
 
 # run the main function
 if __name__ == "__main__":
     main()
+
+
 
